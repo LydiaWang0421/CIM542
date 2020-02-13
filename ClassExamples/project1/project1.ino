3a4a5bfe4f85e6097b0ad6cf5cc9389843bfee49
LydiaWang0421 <54292759+LydiaWang0421@users.noreply.github.com>
2020-02-13 16:16:15 -0500

    0213

--- ClassExamples/project1/project1.ino
+++ ClassExamples/project1/project1.ino
@@ -1,24 +1,48 @@
 int light1 = 2;
 int light2 = 3;
 int boat1 = 4;
+int boat2 = 5;
+
+int pushButton = 8;
 
 int counter = 0;
 
 boolean flipflop = false;
+boolean pressed = false;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  pinMode(pushButton, INPUT);
   
   pinMode(light1, OUTPUT);
   pinMode(light2, OUTPUT);
   pinMode(boat1, OUTPUT);
+  pinMode(boat2, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if(flipflop = false){
+  int buttonState = digitalRead(pushButton);
+
+  Serial.print("buttonState: ");
+  Serial.print(buttonState);
+  delay(1);
+
+  if(buttonState == HIGH){
+    pressed = true;
+  }
+
+  if(buttonState == LOW && pressed == true){
+    pressed = false;
+    Serial.println(" Pressed");
+
+    counter++;
+  }
+
+    if(flipflop = false){
     counter++;
   }else{
     counter--;
@@ -32,18 +56,40 @@ void loop() {
     flipflop = false;
   }
 
-  Serial.println(counter);
+  Serial.print("counter");
+  Serial.print(counter);
 
-  analogWrite(light1, HIGH);
-  delay(1500);
-  analogWrite(light2, HIGH);
-  delay(1500);
-  analogWrite(light1, LOW);
-  delay(200);
-  analogWrite(light2, LOW);
-  delay(200);
-  analogWrite(boat1, HIGH);
-  delay(500);
-  analogWrite(boat1, LOW);
-  delay(500);
+  Serial.println();
+
+  if(counter == 0){
+    analogWrite(light1, LOW);
+    analogWrite(light2, LOW);
+    analogWrite(boat1, LOW);
+    analogWrite(boat2, LOW);
+  }else if(counter == 1){
+    analogWrite(light1, HIGH);
+    analogWrite(light2, HIGH);
+    analogWrite(boat1, HIGH);
+    analogWrite(boat2, HIGH);
+  }else if(counter == 2){
+    analogWrite(light1, HIGH);
+    delay(1500);
+    analogWrite(light2, HIGH);
+    delay(1500);
+    analogWrite(light1, LOW);
+    delay(200);
+    analogWrite(light2, LOW);
+    delay(200);
+    analogWrite(boat1, HIGH);
+    delay(500);
+    analogWrite(boat1, LOW);
+    delay(500);
+    analogWrite(boat2, HIGH);
+    delay(500);
+    analogWrite(boat2, LOW);
+    delay(500);
+  }else{
+    counter = 0;
+  }
+
 }
