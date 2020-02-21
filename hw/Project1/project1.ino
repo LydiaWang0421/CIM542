5a81534c1cefaebf8fac8e8f2579de74671dfb5d
LydiaWang0421 <54292759+LydiaWang0421@users.noreply.github.com>
2020-02-20 20:58:23 -0500

    0220

--- hw/Project1/project1.ino
+++ hw/Project1/project1.ino
@@ -1,16 +1,14 @@
-const int light1 = 2;
-const int light2 = 3;
-const int boat1 = 4;
-const int boat2 = 5;
+ int pushButton = 2;
 
-int pushButton = 8;
+int light1 = 3;
+int light2 = 4;
+int boat1 = 5;
+int boat2 = 6;
+
+//boolean flipflop = false;
+boolean pressed = false;
 
 int counter = 0;
-long prevMillis = 0;
-int interval = 10;
-
-boolean flipflop = false;
-boolean pressed = false;
 
 void setup() {
   // put your setup code here, to run once:
@@ -39,63 +37,62 @@ void loop() {
 
   if(buttonState == LOW && pressed == true){
     pressed = false;
-    Serial.println(" Pressed");
+    Serial.print(" Pressed");
 
     counter++;
   }
 
+  Serial.print("counter");
+  Serial.print(counter);
+
+  Serial.println(); 
   
-  if(millis() - prevMillis >= interval){
-    prevMillis = millis();
-    if(flipflop == false){
-      counter++;
-    }else{
-      counter--;
-    }
-    Serial.println(counter);
+  
+
+//    analogWrite(light1, counter);
+//    analogWrite(light2, counter);
+//    analogWrite(boat1, counter);
+//    analogWrite(boat2, counter);
+//
+//    if(counter >= 255){
+//      flipflop = true;
+//    }
+//    if(counter <= 0){
+//      flipflop = false;
+//    }
+
+  if(counter == 0){
+    digitalWrite(light1, LOW);
+    digitalWrite(light2, LOW);
+    digitalWrite(boat1, LOW);
+    digitalWrite(boat2, LOW);
+  }else if(counter == 1){
+    digitalWrite(light1, HIGH);
+    digitalWrite(light2, HIGH);
+    digitalWrite(boat1, HIGH);
+    digitalWrite(boat2, HIGH);
+  }else if(counter == 2){
+    digitalWrite(light1, HIGH);
+    delay(1500); 
+
+
+    
+    digitalWrite(light2, HIGH);
+    delay(1500);
+    digitalWrite(light1, LOW);
+    delay(200);
+    digitalWrite(light2, LOW);
+    delay(200);
+    digitalWrite(boat1, HIGH);
+    delay(500);
+    digitalWrite(boat1, LOW);
+    delay(500);
+    digitalWrite(boat2, HIGH);
+    delay(500);
+    digitalWrite(boat2, LOW);
+    delay(500);
+  }else{
+    counter = 0;
   }
-
-    analogWrite(light1, counter);
-    analogWrite(light2, counter);
-    analogWrite(boat1, counter);
-    analogWrite(boat2, counter);
-
-    if(counter >= 255){
-      flipflop = true;
-    }
-    if(counter <= 0){
-      flipflop = false;
-    }
-
-//  if(counter == 0){
-//    analogWrite(light1, LOW);
-//    analogWrite(light2, LOW);
-//    analogWrite(boat1, LOW);
-//    analogWrite(boat2, LOW);
-//  }else if(counter == 1){
-//    analogWrite(light1, HIGH);
-//    analogWrite(light2, HIGH);
-//    analogWrite(boat1, HIGH);
-//    analogWrite(boat2, HIGH);
-//  }else if(counter == 2){
-//    analogWrite(light1, HIGH);
-//    delay(1500);
-//    analogWrite(light2, HIGH);
-//    delay(1500);
-//    analogWrite(light1, LOW);
-//    delay(200);
-//    analogWrite(light2, LOW);
-//    delay(200);
-//    analogWrite(boat1, HIGH);
-//    delay(500);
-//    analogWrite(boat1, LOW);
-//    delay(500);
-//    analogWrite(boat2, HIGH);
-//    delay(500);
-//    analogWrite(boat2, LOW);
-//    delay(500);
-//  }else{
-//    counter = 0;
-//  }
 
 }
